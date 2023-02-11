90dba1540a1630887fb75b37b82335cc3b143ce3
Martin Caceres <martincaceres@outlook.com>
2023-02-11 14:11:59 -0300

    improvement

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
 import './index.css';
